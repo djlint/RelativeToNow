d9eaa0c2cf87cd9a4b00de053f7d74f80d0c619e
Christopher Pickering <christopher.pickering@keemail.me>
2021-01-18 15:24:43 -0600

    updated rev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", mode="r", encoding="utf-8") as f:
 
 setup(
     name="relative_to_now",
-    version="0.0.4",
+    version="0.0.5",
     description="Print a datetimes distance from now",
     long_description=readme,
     long_description_content_type="text/markdown",
